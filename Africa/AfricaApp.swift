66072064ca5a7ba396a122d2fcb2d02fcc97ae08
joao <joao.camargo@gmail.com>
2021-07-17 09:24:55 -0300

    first commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by joao camargo on 17/07/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
