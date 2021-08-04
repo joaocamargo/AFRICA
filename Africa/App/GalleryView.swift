865213fab8d312d1b064426e1d0497292760c113
joao <joao.camargo@gmail.com>
2021-08-03 23:33:31 -0300

    aula 41 terminada

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,15 +8,73 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    @State private var selectedAnimal: String = "lion"
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+//    let gridLayout: [GridItem] = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible())
+//    ]
+    
+    //eficient layout
+    //let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+    
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            VStack {
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, perform: { value in
+                        gridSwitch()
+                    })
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10){
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                            //.frame(width: 60, height: 60, alignment: .center)
+                    }
+                } // grid
+                .animation(.easeIn)
+                .onAppear(perform: gridSwitch)
+            } //vstack
+            .padding(.horizontal, 10)
+            .padding(.vertical,50)
+            
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
     }
 }
 
 struct GalleryView_Previews: PreviewProvider {
+    
+    
     static var previews: some View {
         GalleryView()
     }
---
+++ Africa/View/AnimalGridItemView.swift
@@ -0,0 +1,28 @@
+//
+//  AnimalGridItemView.swift
+//  Africa
+//
+//  Created by joao camargo on 03/08/21.
+//
+
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    
+    let animal : Animal
+    
+    var body: some View {
+        Image(animal.image)
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(12)
+    }
+}
+
+struct AnimalGridItemView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        AnimalGridItemView(animal: animals[0]).previewLayout(.sizeThatFits).padding()
+    }
+}
